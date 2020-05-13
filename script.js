0bdd7ebe4cb92da092ad3d1161f95d4ba735b23c
soni <yorikyorik25@gmail.com>
2020-05-13 14:39:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('button'),
+        overlay = document.querySelector('.overlay');
+
+// btn.onclick = function() {
+//     alert('Click')
+// };
+
+// btn.addEventListener('click', () => {
+//     alert('Click');
+// });
+let i = 0;
+const deleteElement = (e) => {
+    console.log(e.currentTarget); 
+    console.log(e.type); 
+    // i++;
+    // if (i ==1) {
+    //     btn.removeEventListener('click', deleteElement);
+    // }   
+};
+
+
+btn.addEventListener('click', deleteElement);
+overlay.addEventListener('click', deleteElement);
+
+const link = document.querySelector('a');
+link.addEventListener('click', function(event){
+    event.preventDefault();
+
+    console.log(event.target);
+});
+    
+
+
